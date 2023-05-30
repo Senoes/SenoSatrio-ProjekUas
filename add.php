b125d1243f0afe534169f3023061fdcc100a396c
Seno Satrio <133359156+Senoes@users.noreply.github.com>
2023-05-30 16:09:49 +0700

    add.php

--- add.php
+++ add.php
@@ -1,24 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tambah Data</title>
-    <link rel="stylesheet" type="text/css" href="style2.css">
+    <title>Halaman Tambah Data</title>
+    <style>
+        .form-container {
+            width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #f2f2f2;
+            border-radius: 5px;
+        }
+
+        .form-group {
+            margin-bottom: 15px;
+        }
+
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        .form-group input[type="text"],
+        .form-group input[type="email"],
+        .form-group input[type="noHP"] {
+            width: 100%;
+            padding: 8px;
+            border-radius: 4px;
+            border: 1px solid #ccc;
+        }
+
+        .form-group button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .form-group button:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h2>Tambah Data</h2>
-
-    <form method="POST" action="insert.php">
-        <label for="nama">Nama:</label>
-        <input type="text" name="nama" required>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-
-        <label for="noHP">no HP:</label>
-        <input type="integer" name="noHP" required>
-
-        <input type="submit" value="Tambah" class="btn">
-    </form>
+    <div class="form-container">
+        <h2>Tambah Data</h2>
+        <form method="POST" action="insert.php">
+            <div class="form-group">
+                <label for="nama">Nama:</label>
+                <input type="text" id="nama" name="nama" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="noHP">no HP:</label>
+                <input type="text" id="noHP" name="noHP" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" name="submit">Tambah</button>
+            </div>
+            </form>
+    </div>
 </body>
 </html>
+
 
